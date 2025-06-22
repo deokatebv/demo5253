3a4879370fd85c1c1f85632f5e4379752e018f81
Bhramadeo <sbvdeokate@gmail.com>
2025-06-22 12:13:14 +0530

    this file in feture to  dev

--- add.py
+++ add.py
@@ -1,12 +1,1 @@
-def add_numbers(a, b):
-    """
-    This function takes two numbers as input and returns their sum.
-    """
-    return a + b
-
-# Example usage
-num1 = float(input("Enter the first number: "))
-num2 = float(input("Enter the second number: "))
-
-result = add_numbers(num1, num2)
-print(f"The sum of {num1} and {num2} is {result}.")
+print("This is  in aaddition part  ")
